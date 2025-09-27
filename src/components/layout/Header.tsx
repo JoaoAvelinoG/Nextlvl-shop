9745ff5a6acdfa943bec72296396c42c757eb306
João Guilherme Avelino <joaoguilhermy0147@gmail.com>
2025-09-27 14:58:59 -0300

    Inicio estilização do menu Mobile

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,14 +3,28 @@
 import Image from "next/image";
 import { Container } from "./Container";
 import { FreightBar } from "./FreightBar";
-import { Button } from "../base/Button";
-import { MenuIcon, ShoppingBagIcon, UserIcon } from "lucide-react";
+import {
+  ArrowUpRightIcon,
+  MenuIcon,
+  ShoppingBagIcon,
+  UserIcon,
+} from "lucide-react";
 import Link from "next/link";
 import { HeaderIcon } from "./HeaderIcon";
 
 type HeaderProps = React.ComponentPropsWithRef<"header">;
 
+type menuItemsListType = {
+  label: string;
+  href: string;
+};
+
 export const Header = ({ ...props }: HeaderProps) => {
+  const menuItemsList: menuItemsListType[] = [
+    { label: "Camisa", href: "/categories/camisa" },
+    { label: "Kit", href: "/categories/kits" },
+  ];
+
   return (
     <>
       <FreightBar />
@@ -30,9 +44,24 @@ export const Header = ({ ...props }: HeaderProps) => {
 
               <HeaderIcon icon={<ShoppingBagIcon size={19} />} href="/cart" />
 
-              <HeaderIcon icon={<MenuIcon size={19} />} />
+              <HeaderIcon className="md:hidden" icon={<MenuIcon size={19} />} />
             </div>
           </div>
+        </Container>
+        <div className="flex flex-col md:hidden">
+          {menuItemsList.map((item) => (
+            <Link key={item.label} href={item.href}>
+              <div className="flex justify-between items-center p-6 text-sm font-medium border-b border-gray-200">
+                {item.label}
+                <div>
+                  <ArrowUpRightIcon className="text-blue-700" size={16} />
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div>
+        <Container>
+          <div className="p-6 bg-red-300 md:hidden">Busca Mobilne</div>
         </Container>
       </header>
     </>
--- src/components/layout/HeaderIcon.tsx
+++ src/components/layout/HeaderIcon.tsx
@@ -4,14 +4,19 @@ import { Button } from "../base/Button";
 type HeaderIconProps = {
   icon: React.ReactNode;
   href?: string;
+  className?: string;
 };
 
-export const HeaderIcon = ({ icon, href = "/" }: HeaderIconProps) => {
+export const HeaderIcon = ({
+  icon,
+  href = "/",
+  className,
+}: HeaderIconProps) => {
   return (
-    <>
+    <div className={className}>
       <Button variant="outline" size="icon">
         <Link href={href}>{icon}</Link>
       </Button>
-    </>
+    </div>
   );
 };
